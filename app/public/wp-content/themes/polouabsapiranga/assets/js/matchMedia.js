8445191d4e506c2ac81959243c4ccfddbd974b60
23232x <23232x@gmail.com>
2023-02-13 18:02:18 -0300

    styling the menu

--- app/public/wp-content/themes/polouabsapiranga/assets/js/matchMedia.js
+++ app/public/wp-content/themes/polouabsapiranga/assets/js/matchMedia.js
@@ -4,3 +4,13 @@ const mediaQueryList = window.matchMedia("(max-width:800px)");
 if (mediaQueryList.matches) {
     console.log("funfou");
 }
+
+
+
+window.matchMedia("(min-width: 400px)").addListener(function (e) {
+    if (e.matches) {
+        console.info("a porta de visualização é de pelo menos 400 pixels de largura");
+    } else {
+        console.info("a porta de visualização não tem pelo menos 400 pixels de largura");
+    }
+}); 
